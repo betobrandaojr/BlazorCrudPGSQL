b7a228519b39101cf4ef8492f86f003fdbeba99d
betobrandaojr <paulo.199706@live.com>
2021-01-18 20:19:57 -0300

    Inventando Models☜(ﾟヮﾟ☜)

---
+++ BlazorCrudPGSQL/Shared/Modals/Animal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorCrudPGSQL.Shared.Modals
+{
+    public class Animal
+    {
+        public long Id { get; set; }
+        public string Especie { get; set; }
+        public decimal Preco { get; set; }
+    }
+}
